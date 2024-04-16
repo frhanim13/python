1572c5dd5203fa851326b67749f79dfd29ef4076
frhanim13 <48370531+frhanim13@users.noreply.github.com>
2024-04-16 15:07:05 +0800

    Update pt.py

--- Analyzing Marketing Campaigns with pandas/pt.py
+++ Analyzing Marketing Campaigns with pandas/pt.py
@@ -8,4 +8,25 @@ marketing = pd.read_csv('marketing.csv')
 # Print the first five rows of the DataFrame
 print(marketing.head(5))
 
+# Print the statistics of all columns
+print(marketing.describe())
+
+# Check column data types and non-missing values
+print(marketing.info())
+
+# Check the data type of is_retained
+print(marketing['is_retained'].dtype)
+
+# Convert is_retained to a boolean
+marketing['is_retained'] = marketing['is_retained'].astype(bool)
+
+# Check the data type of is_retained, again
+print(marketing['is_retained'].dtype)
+
+# Mapping for channels
+channel_dict = {"House Ads": 1, "Instagram": 2, 
+                "Facebook": 3, "Email": 4, "Push": 5}
+
+# Map the channel to a channel code
+marketing['channel_code'] = marketing['subscribing_channel'].map(channel_dict)
 
