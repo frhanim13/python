d3b7b7965f92c838f3ff8750dcca40c4d059ebc9
frhanim13 <48370531+frhanim13@users.noreply.github.com>
2024-04-16 14:55:06 +0800

    Update and rename pt to pt.py

---
+++ Analyzing Marketing Campaigns with pandas/pt.py
@@ -0,0 +1,11 @@
+
+# Import pandas into the environment
+import pandas as pd
+
+# Import marketing.csv 
+marketing = pd.read_csv('marketing.csv')
+
+# Print the first five rows of the DataFrame
+print(marketing.head(5))
+
+
