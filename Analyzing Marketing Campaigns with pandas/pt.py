bc90c1cf2083fff0b699ff048a465e71dc66dfe9
frhanim13 <48370531+frhanim13@users.noreply.github.com>
2024-04-16 15:20:05 +0800

    Update pt.py

--- Analyzing Marketing Campaigns with pandas/pt.py
+++ Analyzing Marketing Campaigns with pandas/pt.py
@@ -30,3 +30,13 @@ channel_dict = {"House Ads": 1, "Instagram": 2,
 # Map the channel to a channel code
 marketing['channel_code'] = marketing['subscribing_channel'].map(channel_dict)
 
+# Import numpy
+numpy as np
+
+# Add the new column is_correct_lang
+marketing['is_correct_lang'] = np.where(marketing['language'] == 'English', True, False)
+
+
+
+
+
