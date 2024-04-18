b16f7a007fc2cb175a01ef6ecc544b85ee3c429f
frhanim13 <48370531+frhanim13@users.noreply.github.com>
2024-04-18 14:31:06 +0800

    Update pt.py

--- Analyzing Marketing Campaigns with pandas/pt.py
+++ Analyzing Marketing Campaigns with pandas/pt.py
@@ -117,6 +117,17 @@ print('English speaker conversion rate:',  round(conversion_rate*100,2), '%')
 English speaker conversion rate: 13.13 %
 
 
+# Group by language_displayed and count unique users
+total = marketing.groupby(['language_displayed'])\
+                                  ['user_id'].nunique()
 
+# Group by language_displayed and count unique conversions
+subscribers = marketing[marketing['converted'] == True]\
+                       .groupby(['language_displayed'])\
+                       ['user_id'].nunique()
+
+# Calculate the conversion rate for all languages
+language_conversion_rate = subscribers/total
+print(language_conversion_rate)
 
 
